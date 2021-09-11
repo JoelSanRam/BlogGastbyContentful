86f57435b81a6695a7def5b1aa7e2c7b9512bd20
Joel Sánchez <troya48_pumas@hotmail.com>
2021-09-11 10:40:01 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,44 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { FluidObject } from "gatsby-image"
+import React from "react";
+import { graphql } from "gatsby";
+import { FluidObject } from "gatsby-image";
 
-import { Layout } from "../components/Layout"
-import { BlogCard } from "../components/BlogCard"
-import { Grid } from "../components/Grid"
-import "../styles/styles.scss"
+import { Layout } from "../components/Layout";
+import { BlogCard } from "../components/BlogCard";
+import { Grid } from "../components/Grid";
+import "../styles/styles.scss";
 
 type HomeProps = {
   data: {
     blogs: {
       edges: {
         node: {
-          id: string
-          title: string
-          slug: string
+          id: string;
+          title: string;
+          slug: string;
           coverImages: {
-            id: string
-            fluid: FluidObject
-          }[]
-        }
-      }[]
-    }
-  }
-}
+            id: string;
+            fluid: FluidObject;
+          }[];
+        };
+      }[];
+    };
+  };
+};
 
 const IndexPage = ({ data: { blogs } }: HomeProps) => {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1>Inicio</h1>
       <Grid>
         {blogs.edges.map(({ node }) => (
           <BlogCard key={node.id} blog={node} />
         ))}
       </Grid>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 export const query = graphql`
   query {
     blogs: allContentfulBlogs {
@@ -57,4 +57,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
